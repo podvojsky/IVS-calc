51e9d39794c81ffae8f49b8720f2cd377927aada
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2023-03-19 16:31:40 +0100

    Define math_lib function headers

--- src/math_lib.py
+++ src/math_lib.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+"""@package math_lib
+This module includes basic mathematical functions.
+
+Each function in this module represents equivalent mathematical function.
+This module can be used for general purposes, but was designed
+to be used in calculator application.
+"""
+
+def add(x: int | float, y: int | float) -> int | float:
+    """Documentation for a function.
+ 
+    More details.
+    """
+    pass
+
+def sub(x: int | float, y: int | float) -> int | float:
+    pass
+
+def mul(x: int | float, y: int | float) -> int | float:
+    pass
+
+def div(x: int | float, y: int | float) -> int | float:
+    pass
+
+def fac(x: int) -> int:
+    pass
+
+def pow(x: int | float, n: int | float) -> int | float:
+    pass
+
+def sqrt(x: int | float, n: int | float) -> int | float:
+    pass
+
+def log(x: int | float, n: int | float) -> int | float:
+    pass
+
+def change_sign(x: int | float) -> int | float:
+    pass
