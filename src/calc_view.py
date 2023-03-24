c1b1181aacc9adc3d8760b82542037eaedb94d79
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2023-03-25 00:59:54 +0100

    Create calculator class, close #6
    Create calculator class and define structure of the application.

--- src/calc_view.py
+++ src/calc_view.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+"""@package calc_view
+This module implements graphical user interface
+for the calculator using tkinter module.
+"""
+
+import tkinter as tk
+
+
+class Calculator:
+    """_summary_
+    """
+
+    def __init__(self) -> None:
+        """_summary_
+        """
+        self.root = tk.Tk()
+        self.root.title("Calculator")
+        self.root.minsize(350, 450)
+
+    def start(self) -> None:
+        """_summary_
+        """
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    calculator.start()
