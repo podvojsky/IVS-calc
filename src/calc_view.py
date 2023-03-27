d6c89d9d9dce137ff3cbff08eb055e44cc48a76b
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2023-03-28 00:18:25 +0200

    Separate window configuration, eprint method
    This commit separates main window configuration from initializer
    method to custom cfg_window() (also sets the icon of the window)
    and adds eprint() method for printing errors to standard error output.

--- src/calc_view.py
+++ src/calc_view.py
@@ -5,24 +5,52 @@ This module implements graphical user interface
 for the calculator using tkinter module.
 """
 
+import sys
 import tkinter as tk
 
 
 class Calculator:
     """_summary_
     """
+    icon_path = "./icons/calculator-96.png"
 
     def __init__(self) -> None:
-        """_summary_
+        """Initializes a new Calculator object.
         """
         self.root = tk.Tk()
-        self.root.title("Calculator")
-        self.root.minsize(350, 450)
+        self.cfg_window()
+        
+    @staticmethod
+    def eprint(e: object) -> None:
+        """Static method for printing errors to standard error output.
+
+        Args:
+            e (object): Any type of exception.
+        """
+        print(e, file=sys.stderr)
 
     def start(self) -> None:
-        """_summary_
+        """Runs the Calculator.
         """
         self.root.mainloop()
+        
+    def cfg_window(self) -> None:
+        """Configures Calculator window parameters.
+        
+        This method configures icon, size, title
+        and other attributes for main Calculator window.
+        """
+        
+        """Setting window icon."""
+        try:
+            icon = tk.PhotoImage(file=Calculator.icon_path)
+            self.root.wm_iconphoto(False, icon)
+        except tk.TclError as icon_not_found:
+            self.eprint(icon_not_found)
+            
+        self.root.title("Calculator")
+        self.root.geometry("350x450")
+        self.root.resizable(width=False, height=False)
 
 
 if __name__ == "__main__":
