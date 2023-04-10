4a4b79567df2f9e46598aea2a52edee505e1742f
Diuturnae <formanekd03@gmail.com>
2023-04-10 10:10:59 +0200

    Added a check for input and a check for execution.

--- src/profiling.py
+++ src/profiling.py
@@ -2,6 +2,7 @@
 import math_lib
 import fileinput
 import re
+import sys
 
 def xavg(l):
     """Function xavg for calculating the average value of x
@@ -24,14 +25,18 @@ def deviation(lst):
     """Function deviation for calculating standard deviation
     input is a list of numbers
     """
-    n=len(lst) #The number of numbers in the input
-
+    n=len(lst) #The number of numbers in the list
+    if n==0:
+        print("No numbers were found in the input")
+        sys.exit()
     tmp5=math_lib.pow(xavg(lst),2) 
     tmp4=math_lib.mul(n,tmp5)                   
-    tmp3=sum(math_lib.pow(i,2) for i in lst)    
+    tmp3=sum(i*i for i in lst)    
     tmp2=math_lib.sub(tmp3,tmp4)                
     tmp1=math_lib.div(tmp2,n-1)                 
-    return math_lib.sqrt(tmp1,2)                
+    return math_lib.sqrt(2,tmp1)                
 
-list=inputf()
-print(deviation(list))
+if __name__ == "__main__":
+    list=inputf()
+    print(deviation(list))
+
