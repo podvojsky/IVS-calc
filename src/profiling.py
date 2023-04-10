f8e4e7ccd86f06c573a628a83bfbd7496bac1232
Diuturnae <formanekd03@gmail.com>
2023-04-10 09:35:12 +0200

    Made equation and input more readable

--- src/profiling.py
+++ src/profiling.py
@@ -2,7 +2,6 @@
 import math_lib
 import fileinput
 import re
-import math
 
 def xavg(l):
     """Function xavg for calculating the average value of x
@@ -14,18 +13,24 @@ def inputf():
     """Function inputf for getting input from stdin
     takes input from stdin and returns a list of numbers found in stdin
     """
-    tmp=[]
+    nline=[]
+    nlist=[]
     for line in fileinput.input():
-        tmp=tmp+(re.findall('[0-9]+',line))
-    return [float(n) for n in tmp]
+        nline=(re.findall('[0-9]+',line))
+        nlist=nlist+nline
+    return [float(n) for n in nlist]
         
 def deviation(lst):
     """Function deviation for calculating standard deviation
     input is a list of numbers
     """
     n=len(lst)    
-    result=math.sqrt(math_lib.div(math_lib.sub(sum(i*i for i in lst),math_lib.mul(n,math.pow(xavg(lst),2))),n-1))   
-    return result
+
+    tmp4=math_lib.mul(n,math_lib.pow(xavg(lst),2))
+    tmp3=sum(math_lib.pow(i,2) for i in lst)
+    tmp2=math_lib.sub(tmp3,tmp4)
+    tmp1=math_lib.div(tmp2,n-1)
+    return math_lib.sqrt(tmp1,2) 
 
 list=inputf()
 print(deviation(list))
