ed708c9086ab592ae84edc2c3245a3a5210e737b
Diuturnae <formanekd03@gmail.com>
2023-04-10 09:39:54 +0200

    Added comments to variables

--- src/profiling.py
+++ src/profiling.py
@@ -13,8 +13,8 @@ def inputf():
     """Function inputf for getting input from stdin
     takes input from stdin and returns a list of numbers found in stdin
     """
-    nline=[]
-    nlist=[]
+    nline=[] #A list of numbers inputted on a line
+    nlist=[] #A list of the inputted numbers
     for line in fileinput.input():
         nline=(re.findall('[0-9]+',line))
         nlist=nlist+nline
@@ -24,13 +24,14 @@ def deviation(lst):
     """Function deviation for calculating standard deviation
     input is a list of numbers
     """
-    n=len(lst)    
+    n=len(lst) #The number of numbers in the input
 
-    tmp4=math_lib.mul(n,math_lib.pow(xavg(lst),2))
-    tmp3=sum(math_lib.pow(i,2) for i in lst)
-    tmp2=math_lib.sub(tmp3,tmp4)
-    tmp1=math_lib.div(tmp2,n-1)
-    return math_lib.sqrt(tmp1,2) 
+    tmp5=math_lib.pow(xavg(lst),2) 
+    tmp4=math_lib.mul(n,tmp5)                   
+    tmp3=sum(math_lib.pow(i,2) for i in lst)    
+    tmp2=math_lib.sub(tmp3,tmp4)                
+    tmp1=math_lib.div(tmp2,n-1)                 
+    return math_lib.sqrt(tmp1,2)                
 
 list=inputf()
 print(deviation(list))
