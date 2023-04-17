7081ffb853f80f42880b3bd7acc79f236a49d622
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2023-04-17 23:23:41 +0200

    Add method for creating and styling buttons

--- src/calc_view.py
+++ src/calc_view.py
@@ -23,6 +23,7 @@ class Calculator:
         self.cfg_window()
         self.create_display()
         self.create_buttons()
+        self.style_buttons()
         
         
     @staticmethod
@@ -62,7 +63,7 @@ class Calculator:
         self.root.configure(bg=c.WINDOW_BG)
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
-        self.root.rowconfigure(1, weight=4)
+        self.root.rowconfigure(1, weight=1)
         
     def create_display(self) -> None:
         """Creates output display.
@@ -77,6 +78,50 @@ class Calculator:
         self.display.configure(state="readonly")
         
         self.display.pack()
+        
+    def create_buttons(self) -> None:
+        """Creates buttons for numbers and functions.
+        """
+        
+        def create_button(btn_label: str, grid_position: tuple[int, int]):
+            button = tk.Button(self.buttons_frame, text=btn_label, font=(Calculator.FONT, 20), width=2, bg=c.NUMBER_BTN_BG, fg=c.NUMBER_BTN_FG)
+            button.grid(row=grid_position[0], column=grid_position[1], padx=2, pady=2)
+            self.buttons[btn_label] = button
+            pass
+        
+        
+        self.buttons_frame = tk.Frame(self.root, bg=c.WINDOW_BG, width=300)
+        self.buttons_frame.grid(row=1, column=0)
+        
+        self.buttons = {}
+        
+        buttons_template = [
+            "C", "x^y", "y√x", "ln", "!",
+            "7", "8", "9", "÷", "√x",
+            "4", "5", "6", "*", "x^2",
+            "1", "2", "3", "-", "=",
+            "+/-", "0", ".", "+"
+        ]
+        
+        for i, btn_label in enumerate(buttons_template):
+            create_button(btn_label, (i // 5, i % 5))
+            
+        self.buttons["="].grid(rowspan=2)
+        self.buttons["="].config(height=2, pady=13)
+        
+        print(self.buttons)
+        
+        pass
+    
+    def style_buttons(self):
+        self.buttons["="].configure(bg=c.EQ_BTN_BG, fg=c.EQ_BTN_FG)
+        function_buttons = ["C", "x^y", "y√x", "ln", "!", "√x", "x^2"]
+        operator_buttons = ["+", "-", "*", "÷", ".", "+/-"]
+        for fn_btn in function_buttons:
+            self.buttons[fn_btn].configure(bg=c.FUNCTION_BTN_BG, fg=c.FUNCTION_BTN_FG)
+        for oper_btn in operator_buttons:
+            self.buttons[oper_btn].configure(bg=c.OPERATOR_BTN_BG, fg=c.OPERATOR_BTN_FG)
+        pass
         
 
 
